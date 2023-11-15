2b10189e441d88a744ea40eb6ed51bceaee5ce01
GabrielMSchmidt <gabrielmarochischmidt@gmail.com>
2023-11-15 17:35:14 -0300

    Arrumando View NovoProduto

--- resources/views/novoProdutoOverview.blade.php
+++ resources/views/novoProdutoOverview.blade.php
@@ -77,10 +77,55 @@
             </div>
 
             <div class="content3">
+              <form action="">
                 @csrf
                 <br>
                 <h3>Cadastrar Novo Produto</h3>
-                <input type="text" placeholder="Nome" name="Produto" class="input">
+                <fieldset>
+                  <div class="campo">
+                    <label for="nomeProduto">Nome</label>
+                    <input type="text" placeholder="Nome" name="nomeProduto" id="nomeProd" required>
+                    
+                    <label for="codProduto">Código</label>
+                    <input type="number" placeholder="Código" name="codProduto" id="codigoProd" required>
+                  </div>
+
+                  <div class="campo">
+                    <label for="categoriaProduto">Categoria</label>
+                    <input type="text" placeholder="Categoria" name="categoriaProduto" id="categoriaProd" required>
+                    
+                    <label for="dataProduto">Data</label>
+                    <input type="date" name="dataProduto" id="dataProd" required>
+                  </div>
+
+                  <div class="campo">
+                    <label for="qtdeProduto">Quantidade</label>
+                    <input type="number" placeholder="Quantidade" name="qtdeProduto" id="qtdeProd" required>
+                    
+                    <label for="preçoProduto">Preço Unitário</label>
+                    <input type="number" placeholder="Preço Unitário" name="precoProduto" id="precoProd" required>
+                  </div>
+
+                  <div class="campo">
+                    <label for="fornecedorProd">Fornecedor</label>
+                    <select id="fornecedorProd" required>
+                      <option>Fornecedor 1</option>
+                      <option>Fornecedor 2</option>
+                      <option>Fornecedor 3</option>
+                    </select>
+                  </div>
+
+                  <div class="campo">
+                    <label for="cnpjProd">CNPJ</label>
+                    <input type="text" placeholder="CNPJ" name="cnpjProd" id="cnpjProd" required>
+                  </div>
+
+                  <div class="campo">
+                    <button type="menu" class="botao">Cancelar</button>
+                    <button type="submit" class="botao">Salvar</button>
+                  </div>
+              </fieldset>
+              </form>
             </div>
 
         </div>
