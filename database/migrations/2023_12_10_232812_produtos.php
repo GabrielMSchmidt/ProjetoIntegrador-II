0ee87bc62f47b1831b095843c412ad4b30fe010a
GabrielMSchmidt <gabrielmarochischmidt@gmail.com>
2023-12-11 00:45:02 -0300

    Criação das Migrations e Models

---
+++ database/migrations/2023_12_10_232812_produtos.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('codigo');
+            $table->string('categoria');
+            $table->date('data');
+            // $table->number('quantidade');
+            // $table->number('pecoUnitario');
+            $table->string('fornecedor');
+            $table->string('cnpj');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
